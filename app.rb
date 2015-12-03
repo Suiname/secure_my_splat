6def60fd1b2f8d8b438b565b28f699561854c689
Jason Tham <jtham@sbcglobal.net>
2015-12-03 12:24:05 -0600

    commit

--- app.rb
+++ app.rb
@@ -1,2 +1,11 @@
 require 'bundler'
 Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql',
+  :database => 'bee_crypt_bzz'
+)
+
+get '/' do
+  return {:hello => 'world'}.to_json
+end
